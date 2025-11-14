7b298361d85f494530293aa348be9bc99b9035eb
Rossenzii <jangmj80@naver.com>
2025-11-14 18:09:52 +0900

    syntax error

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -xe
+
+DOCKER_IMAGE=$1
+
+echo "[1] .env 파일 생성"
+printf "%s\n%s\n" "$ENV_AZURE" "$ENV_DB" > /home/azureuser/app/.env
+
+echo "[2] Docker 이미지 Pull"
+docker pull $DOCKER_IMAGE
+
+echo "[3] 기존 컨테이너 정리"
+docker stop paju_web || true
+docker rm paju_web || true
+
+echo "[4] 새 컨테이너 실행"
+docker run -d \
+  --name paju_web \
+  --env-file /home/azureuser/app/.env \
+  -p 8000:8000 \
+  -v /home/azureuser/app/media:/app/media \
+  -v /home/azureuser/app/staticfiles:/app/staticfiles \
+  $DOCKER_IMAGE
+
+echo "[5] 배포 완료!"
